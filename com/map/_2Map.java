77cbf73d003dad1c55d30c4612c11804dd5c7740
Taher Ali <taher@ali.org>
2022-07-11 17:16:27 +0530

    add space

--- com/map/_2Map.java
+++ com/map/_2Map.java
@@ -8,7 +8,7 @@ public class _2Map {
 	public void mapOperation() {
 		Map<String, String> names = new HashMap<>();
 		names.put("A", "1");
-		names.put("C", "2"); // fix for issue in bugfix
+		names.put("C", "2"); // fix for issue in bugfix 
 		System.out.println(names);
 	}
 
